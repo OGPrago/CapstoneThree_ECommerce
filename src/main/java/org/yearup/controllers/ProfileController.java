7d47d359dd08059045a1ce3a3671d8d5c4bcd6c7
Jeremy <ogprago@gmail.com>
2024-06-27 10:05:50 -0400

    Completed getProfile()

--- src/main/java/org/yearup/controllers/ProfileController.java
+++ src/main/java/org/yearup/controllers/ProfileController.java
@@ -3,36 +3,51 @@ package org.yearup.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProfileDao;
+import org.yearup.data.UserDao;
 import org.yearup.models.Profile;
+import org.yearup.models.User;
+
+import java.security.Principal;
 
 @RestController
 @RequestMapping("profile")
 @PreAuthorize("isAuthenticated()")
 public class ProfileController
 {
-    private ProfileDao profileDao;
+    private final ProfileDao profileDao;
+    private final UserDao userDao;
 
-    public ProfileController(ProfileDao profileDao)
+    public ProfileController(ProfileDao profileDao, UserDao userDao)
     {
         this.profileDao = profileDao;
+        this.userDao = userDao;
     }
 
-    @GetMapping
-    public Profile getByUserId(@PathVariable int id)
+    @GetMapping("")
+    public Profile getProfile(Principal principal)
     {
         try
         {
-            var product = profileDao.getByUserId(id);
+            // Get the currently logged in username
+            String userName = principal.getName();
 
-            if (product == null)
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+            // Find the user by username
+            User user = userDao.getByUserName(userName);
 
-            return product;
+            if (user == null)
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found.");
+
+            // Use the profileDao to get the user's profile by user ID
+            Profile profile = profileDao.getByUserId(user.getId());
+
+            if (profile == null)
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Profile not found.");
+
+            return profile;
         }
         catch (Exception ex)
         {
